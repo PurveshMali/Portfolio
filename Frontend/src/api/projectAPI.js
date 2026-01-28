d6731328109f5d25a37c1bb68628398066c50620
Purvesh Mali <purveshmali99@gmail.com>
2026-01-28 22:45:32 +0530

    update

--- Frontend/src/api/projectAPI.js
+++ Frontend/src/api/projectAPI.js
@@ -1,6 +1,6 @@
 import api from './axios';
 
 export const getAllProjects = async () => {
-  const response = await api.get('/projects');
+  const response = await api.get('/api/projects');
   return response.data; // { success, data }
 };
